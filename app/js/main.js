0efe7ab0466d70fabad5a9aab8d86e310f568ac9
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2025-02-11 17:48:28 +0200

    add

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   window.addEventListener("scroll", function () {
+//     const header = document.querySelector(".header");
+//     if (window.scrollY > 0) {
+//       header.classList.add("fixed");
+//     } else {
+//       header.classList.remove("fixed");
+//     }
+//   });
+// });
